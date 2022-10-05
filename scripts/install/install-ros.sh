97bb5b552377544ac7aa7f937e5c83e7ed8f67bb
Alexander Marinov <alekmarinov@gmail.com>
2022-10-05 00:34:41 -0400

    rename run -> sudorun in install-ros.sh

--- scripts/install/install-ros.sh
+++ scripts/install/install-ros.sh
@@ -2,7 +2,7 @@
 ROS_DISTRO=humble
 ROS_HOME=/opt/ros/$ROS_DISTRO
 
-run() {
+sudorun() {
     echo "$*"
     sudo $*
     if [[ $? != 0 ]]; then
@@ -11,18 +11,18 @@ run() {
     fi
 }
 
-run apt update -y
-run apt install -y software-properties-common
-run add-apt-repository -y universe
-run apt update -y
-run apt install -y curl gnupg lsb-release
-run curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+sudorun apt update -y
+sudorun apt install -y software-properties-common
+sudorun add-apt-repository -y universe
+sudorun apt update -y
+sudorun apt install -y curl gnupg lsb-release
+sudorun curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 sudo echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(source /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
-run apt update -y
-run apt upgrade -y
-run apt install -y ros-$ROS_DISTRO-desktop python3-roslaunch
+sudorun apt update -y
+sudorun apt upgrade -y
+sudorun apt install -y ros-$ROS_DISTRO-desktop python3-roslaunch
 source "$ROS_HOME/setup.bash"
-run sudo chown -R "$USER:$USER" "$ROS_HOME"
+sudorun sudo chown -R "$USER:$USER" "$ROS_HOME"
 echo "Updating $HOME/.bashrc"
 sed -r -i '/ROS_HOME|ROS_DISTRO/d' ~/.bashrc
 echo "export ROS_HOME=\"$ROS_HOME\"" >> ~/.bashrc
